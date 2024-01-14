3381ec67b82d1ae66065b880934d14043856652b
poopdawguwu <ian@popoheads.com>
2024-01-14 14:30:16 +0800

    added matchload path, removed temporary debug functions

--- src/auton/paths.cpp
+++ src/auton/paths.cpp
@@ -6,14 +6,12 @@ bool calibration(int path){
     Task autonOdomTask(odomTracker, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Odom Task");
     Task autonPIDTask(controlPID, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "PID Task");
     Task autonDebugTask(debugTerminal, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Debug Task");
-    Imu inertial(inertialPort);
     bool success = false;
     controlPIDEnable = true;
 
     switch (path){
         case 0:
-            success = controlMove(24, 3000);
-            printf("success: %d\n\n\n\n\n\n\n", success);
+            success = controlMove(24, 10000);
             break;
 
         case 1:
@@ -35,4 +33,38 @@ bool calibration(int path){
     autonSensorsTask.remove();
 
     return success;
+}
+
+void matchload(){
+    Task autonSensorsTask(sensorsTracker, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Sensors Task");
+    Task autonOdomTask(odomTracker, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Odom Task");
+    Task autonPIDTask(controlPID, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "PID Task");
+    Task autonDebugTask(debugTerminal, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Debug Task");
+	Motor intake(intakePort, false);
+	Motor cata(cataPort, false);
+	ADIDigitalOut wingLeft(wingLeftPort, false);
+	ADIDigitalOut wingRight(wingRightPort, false);
+    controlPIDEnable = true;
+
+    controlMove(32, 2000);
+    controlTurn(45, 1000);
+    intake.move(80);
+    controlMove(5, 100);
+    intake.move(0);
+    controlMove(25, 650);
+    controlMove(-30, 2000);
+    controlTurn(-45, 1250);
+    wingLeft.set_value(true);
+    controlMove(-20, 1000);
+    wingLeft.set_value(false);
+    controlMoveTo(true, 15, -15, 1000, 1000);
+    wingRight.set_value(true);
+    controlMoveTo(true, 23, -23, 1000, 1000);
+
+    delay(250);
+    controlPIDEnable = false;
+    autonDebugTask.remove();
+    autonPIDTask.remove();
+    autonOdomTask.remove();
+    autonSensorsTask.remove();
 }
